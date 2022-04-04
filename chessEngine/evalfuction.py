595b8a793932f56e1f3a0cbc2ea5035d01d9bbe0
abhishek3005-netizen <abhishek.sharma2019@vitstudent.ac.in>
2022-04-05 03:37:00 +0530

    Basic evalfunc

--- chessEngine/evalfuction.py
+++ chessEngine/evalfuction.py
@@ -3,7 +3,7 @@ import chess
 class EvalFunc:
     
     def __init__(self, color) -> None:
-        self.piecevalue = {
+        self.pieceValues = {
                 chess.PAWN: 1.0, 
                 chess.BISHOP: 3.0, 
                 chess.KNIGHT: 2.0, 
@@ -15,4 +15,17 @@ class EvalFunc:
         self.color = color 
 
     def eval(self, board : chess.Board) -> float:
-        pass
+        eval_score = 0.0
+        
+        # a dict of pieces currently on the board
+        pieces = board.piece_map()
+        
+        # pieces -> Dict[chess.Square, chess.Piece]
+        
+        for piece in pieces.values():
+            if(i.color == self.color):
+                eval_score += self.pieceValues[piece.piece_type]
+            else:
+                eval_score -= self.pieceValues[piece.piece_type]
+                
+        return eval_score
